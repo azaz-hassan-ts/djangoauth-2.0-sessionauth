432ef65dc0a28b8f637f76af9f2e2f2e1b84f68f
azaz-hassan-ts <86603507+azaz-hassan-ts@users.noreply.github.com>
2021-07-13 15:09:56 +0500

    Added swagger documentation

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,28 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import url
+from . import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+app_name = "api"
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Session Auth Api",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.sessionauth.com/policies/terms/",
+        contact=openapi.Contact(email="azaz.hassan@techno-soft.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
-    path(),
-    
+    url(
+        "^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"
+    ),
 ]
